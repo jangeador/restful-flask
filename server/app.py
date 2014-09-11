bec6406ba5d5ba07fe34ff902009b15e5f7383c9
Delio Castillo <jangeador@gmail.com>
2014-09-11 04:37:29 -0700

    PEP-8

--- server/app.py
+++ server/app.py
@@ -7,21 +7,22 @@ tasks = [
     {
         'id': 1,
         'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
         'done': False
     },
     {
         'id': 2,
         'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
+        'description': u'Need to find a good Python tutorial on the web',
         'done': False
     }
 ]
 
 # @app.route('/todo/api/v1.0/tasks', methods = ['GET'])
-@app.route('/tasks', methods = ['GET'])
+@app.route('/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify( { 'tasks': tasks } )
+    return jsonify({'tasks': tasks})
+
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
