b88328f5b42b2a0b81f9d2482d5eba349deca77c
Delio Castillo <jangeador@gmail.com>
2014-09-11 12:18:12 -0700

    fixes an issue with extra code on services.js from dev. Also, adds tasks.tasks to adjust for the format of json on the web service.

--- client/js/services.js
+++ client/js/services.js
@@ -3,10 +3,3 @@ var clientServices = angular.module('clientServices', ['ngResource']);
 clientServices.factory('Task', ['$resource', function ($resource) {
     return $resource("http://127.0.0.1:5000/tasks");
 }]);
-
-batchModule.factory('routeTemplateMonitor', ['$route', 'batchLog', '$rootScope',
-    function ($route, batchLog, $rootScope) {
-        $rootScope.$on('$routeChangeSuccess', function () {
-            batchLog($route.current ? $route.current.template : null);
-        });
-    }]);
