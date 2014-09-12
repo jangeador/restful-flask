613da7405db57c0bd30da4eacf6fa5b08e0c806d
Delio Castillo <jangeador@gmail.com>
2014-09-11 17:06:25 -0700

    fixes checkboxes

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -4,12 +4,12 @@ var ws="http://ws.gosanluis.com/tasks"
 
 clientControllers.controller("TaskListCtrl", ['$scope', '$http', function ($scope, $http) {
     $http.get(ws).success(function (data) {
-        $scope.tasks = data;
+        $scope.tasks = data.tasks;
         //$scope.msg = "Ehlo"
 
         function done(task, taskState){
             task.done = taskState;
-            $scope.msg = 'clicked ' + task.title;
+            $scope.msg = 'clicked ' + task.title + ' ' + taskState;
         };
 
         $scope.done = done;
